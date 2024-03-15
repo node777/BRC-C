e27829cb8cd3f3b3f9eeab5a3c3ed401066fd66f
overlorb <contact@lorb.net>
2024-03-14 17:12:53 -0700

    added botz

---
+++ tools/generator.js
@@ -0,0 +1,339 @@
+let bots=[]
+
+async function loadBots(){
+    let inscriptionList= await fetch("./inscriptions.json")
+    inscriptionList = await inscriptionList.json()
+    let blockList= await fetch("./blocks.json")
+    blockList = await blockList.json()
+    for(i in inscriptionList){
+        bots[i]={
+            id:inscriptionList[i],
+            block:blockList[i],
+            traits:generateTraits(blockList[i])
+        }
+    }
+    let botHTML=``
+    for(b in bots){
+        botHTML+=drawBot(b)
+    }
+    document.getElementById("bots").innerHTML=botHTML
+
+}
+function drawBot(num){
+    let traits=bots[num].traits
+    return `
+        <div class="bot">
+            <img style="top:${num*272}px" class="piece" src="./parts/backgrounds/${traits.backgrounds}.png" />
+            <img style="top:${num*272}px" class="piece" src="./parts/bodies/${traits.bodies}.png" />
+            <img style="top:${num*272}px" class="piece" src="./parts/clothing/${traits.clothing}.png" />
+            <img style="top:${num*272}px" class="piece" src="./parts/accessories/${traits.accessories}.png" />
+            <img style="top:${num*272}px" class="piece" src="./parts/eyes/${traits.eyes}.png" />
+            <img style="top:${num*272}px" class="piece" src="./parts/mouths/${traits.mouths}.png" />
+        </div>
+    `
+}
+function ruleQualifies(rules, digits){
+    for(rule in rules){
+        if(Number(digits[rule])==rules[rule]){
+            return true
+        }else{return false}
+    }
+}
+function getTrait(traitType, block){
+    let digits = block.toString().split("")
+    let trait=""
+    for(part in parts[traitType]){
+        let partData = parts[traitType][part]
+        if(partData.default==true){
+            trait=part
+        }else if(ruleQualifies(partData.rules, digits)){
+            return part
+        }
+    }
+    return trait
+
+}
+function generateTraits(block){
+    let traits={}
+    for(traitType in parts){
+        // traits[traitType]
+        traits[traitType]=getTrait(traitType, block)
+    }
+    return traits
+}
+loadBots()
+let parts = {
+    backgrounds:{
+        "none":{
+            default:true
+        },
+        "city":{
+            rules:{
+                5:7
+            }
+        },
+        "dmt":{
+
+        },
+        "dungeon":{
+
+        },
+        "gold":{
+
+        },
+        "mechanic":{
+
+        },
+        "stars":{
+
+        },
+    },
+    bodies:{
+        "golden":{
+            default:true
+
+        },
+        "blue":{
+            rules:{
+                4:7
+            }
+
+        },
+        "cursed":{
+            rules:{
+                4:6
+            }
+
+        },
+        "hybrid":{
+            rules:{
+                4:5
+            }
+
+        },
+        "purple":{
+            rules:{
+                4:4
+            }
+
+        },
+        "unbot":{
+            rules:{
+                4:3
+            }
+
+        },
+        "vbot":{
+            rules:{
+                4:2
+            }
+
+        },
+
+    },
+    accessories:{
+        "classic":{
+            default:true
+            
+        },
+        "chain":{
+
+        }
+    },
+    borg:{
+        "classic":{
+            default:true
+            
+        },
+        "borg1":{
+            
+        },
+        "borg2":{
+            
+        },
+    },
+    clothing:{
+        "classic":{
+            default:true
+            
+        },
+        "blackT":{
+            
+        },
+        "bladeJacket":{
+            
+        },
+        "cursedJacket":{
+            
+        },
+        "hoodie":{
+            
+        },
+        "whiteT":{
+            
+        },
+        "whiteTSimple":{
+            
+        },
+    },
+    eyes:{
+        "bitmapVR":{
+            default:true
+            
+        },
+        "bitrunner":{
+            
+        },
+        "blueXVisor":{
+            
+        },
+        "blurryBlue":{
+            
+        },
+        "blurryOrange":{
+            
+        },
+        "classicXVisor":{
+            
+        },
+        "cursedVR":{
+            
+        },
+        "dmtVR":{
+            
+        },
+        "eliteShades":{
+            
+        },
+        "eyePatch":{
+            
+        },
+        "fud":{
+            
+        },
+        "laser":{
+            rules:{
+                3:9
+            }
+            
+        },
+        "neoTokyo":{
+            rules:{
+                3:8
+            }
+            
+        },
+        "orange":{
+            rules:{
+                3:7
+            }
+            
+        },
+        "ordi":{
+            rules:{
+                3:6
+            }
+            
+        },
+        "ordiBlue":{
+            rules:{
+                3:0
+            }
+            
+        },
+        "ordinalThug":{
+            rules:{
+                3:0
+            }
+            
+        },
+        "oversized":{
+            rules:{
+                3:0
+            }
+            
+        },
+        "purple":{
+            
+            rules:{
+                3:5
+            }
+            
+        },
+        "redTriclops":{
+            rules:{
+                3:4
+            }
+            
+        },
+        "stoned":{
+            rules:{
+                3:3
+            }
+            
+        },
+        "triclops":{
+            rules:{
+                3:2
+            }
+            
+        },
+        "XPVisor":{
+            rules:{
+                3:1
+            }
+            
+        },
+        "yellowShades":{
+            rules:{
+                3:0
+            }
+            
+        }
+    },
+    hats:{
+        "none":{
+            default:true
+            
+        },
+        "bonnie":{
+            
+        },
+        "circuits":{
+            
+        },
+        "cursed":{
+            
+        },
+        "daft":{
+            
+        },
+        "pirateDjinn":{
+            
+        }
+    },
+    mouths:{
+        "classic":{
+            default:true
+            
+        },
+        "blunt":{
+            
+        },
+        "bubblegum":{
+            
+        },
+        "laugh":{
+            
+        },
+        "pirate":{
+            
+        },
+        "rainbow":{
+            
+        },
+        "toungue":{
+            
+        }
+    }
+}
